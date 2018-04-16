c9a0183662e8d2f762efb1edbc5d06b3e33c7493
DylanCordoza <dylan.cordoza@gmail.com>
2018-04-15 19:11:59 -0700

    mapCode.cc Final

--- mapCode.cc
+++ mapCode.cc
@@ -2,37 +2,43 @@
 #include <ncurses.h>
 using namespace std;
 
-void printMap(int lvl){
-
-	string t7 = " #_#_#_#_# \n";
-	string t6 = "/_________\\ ";
-	string t5 = " |_______| \n";
-	string t4 = " |_______| \n";
-	string t3 = " |_______| \n";
-	string t2 = " |_______| \n";
-	string t1 = " |_______| \n";
-
-	if (lvl == 1) t1.at(5) = 'X';
-	if (lvl == 2) t2.at(5) = 'X';
-	if (lvl == 3) t3.at(5) = 'X';
-	if (lvl == 4) t4.at(5) = 'X';
-	if (lvl == 5) t5.at(5) = 'X';
-		
-	cout << t7 << t6 << endl << t5 << t4 << t3 << t2 << t1;
-
-	
+void clrscr() {
+	cout << "\033[2J\033[1;1H";
 }
 
 
-int main() {
-		//printMap(1);
+void printMap(int lvl) {
 
-cout << "|--|__|--|__|--|__|--|__|--|__|--|__| \n|                                   | \n| 			            | \n |_________________________________| \n \				   / \n /	 			   \\ \n |                                 | \n |_________________________________| \n \                                 / \n /                                 \\ \n | 			           | \n |_________________________________| \n \                                 / \n /                                 \\ \n | 		                   | \n |_________________________________| \n \                                 / \n /                                 \\ \n | 		  	           | \n |_________________________________| \n \                                 / \n /                                 \\ \n | 		       	           | \n_|_________________________________|______ \n";
+	string emptyflr = " \\                                 / \n /                                 \\ \n | 			           | \n |_________________________________| \n";
+	string emptyflrgr = " \\                                 / \n /                                 \\ \n | 			           | \n_|_________________________________|______ \n";
+	string roof = "|--|__|--|__|--|__|--|__|--|__|--|__| \n|                                   | \n| 			            | \n |_________________________________| \n";
+	string plrflr = " \\                                 / \n /                @                \\ \n | 		 /H\\               | <-- You are here\n |_______________/_\\_______________| \n";
+	string plrflrgr = " \\                                 / \n /                @                \\ \n | 		 /H\\               | <-- You are here\n_|_______________/_\\_______________|______\n";
 
+	string t6 = roof;
+	string t5 = emptyflr;
+	string t4 = emptyflr;
+	string t3 = emptyflr;
+	string t2 = emptyflr;
+	string t1 = emptyflrgr;
+
+	if (lvl == 1) t1 = plrflrgr;
+	if (lvl == 2) t2 = plrflr;
+	if (lvl == 3) t3 = plrflr;
+	if (lvl == 4) t4 = plrflr;
+	if (lvl == 5) t5 = plrflr;
+	cout << t6 << t5 << t4 << t3 << t2 << t1;
+
+	cout << "To exit, type e and press ENTER\n";
+
+	while (true) {
+		string s = "";
+		cin >> s;
+		if (s == "e") break;
+	}
+	clrscr();
 
 }
-
-
 
 
 
